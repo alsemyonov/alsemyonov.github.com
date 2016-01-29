8c5120009c79508dbb6c2250c21da30f82930679
Alex Semyonov <al@semyonov.us>
2016-01-29 07:52:50 +0300

    Move blog articles into subdirectories

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ activate :blog do |blog|
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.sources = '{year}/{month}/{day}/{title}.html'
   # blog.taglink = "tags/{tag}.html"
   blog.layout = 'post'
   # blog.summary_separator = /(READMORE)/
