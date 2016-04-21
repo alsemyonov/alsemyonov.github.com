08b0ff99558b8df1071104f95e329f8e9e563cfd
Alex Semyonov <al@semyonov.us>
2016-04-21 19:29:45 +0300

    Add ya.browserUpgrader

--- source/javascripts/screen.js
+++ source/javascripts/screen.js
@@ -8,4 +8,24 @@ $(function () {
 
   var root = document.getElementsByClassName ? document.getElementsByClassName('b-contents__wrapper')[0] : document.body;
   ya.yobject.load(root, { domainSearch: 'https://al.semyonov.us/search/?searchid=2252973&text={0}&web=0' });
+
+  var yaBrowserUpdater = new ya.browserUpdater.init({
+    "lang": "ru",
+    "browsers": {
+      "yabrowser": "15.12",
+      "chrome": "48",
+      "ie": "10",
+      "opera": "34",
+      "safari": "8",
+      "fx": "43",
+      "amigo": "Infinity",
+      "iron": "35",
+      "flock": "Infinity",
+      "palemoon": "25",
+      "camino": "Infinity",
+      "maxthon": "4.5",
+      "seamonkey": "2.3"
+    },
+    "theme": "yellow"
+  });
 });
